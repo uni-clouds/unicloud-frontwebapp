ffd6ecd13649c32da48c96a49f47e2f443860db4
ManuCoutinho <gdn.sasha@gmail.com>
2022-05-18 18:47:36 -0300

    created submit button component

--- src/components/Buttons/SubmitButton.tsx
+++ src/components/Buttons/SubmitButton.tsx
@@ -1,3 +1,14 @@
-export const SubmitButton: React.FC = () => {
-  return <button></button>
+import { SubmitButtonProps } from './types'
+
+// todo criar spinner de loading no text
+
+export const SubmitButton: React.FC<SubmitButtonProps> = ({ text }) => {
+  return (
+    <button
+      type='submit'
+      className='w-100 text-base bg-brand-700 px-6 py-2.5 text-neutral-50 font-bold leading-tight rounded-md border-brand-700'
+    >
+      {text}
+    </button>
+  )
 }
