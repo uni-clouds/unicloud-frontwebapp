6a2803fdac5b1bbd1293a7067b20b7ab5a5e2944
ManuCoutinho <gdn.sasha@gmail.com>
2022-05-25 12:11:38 -0300

    :recycle: updated Divider Component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,7 +3,7 @@ import { NavHeader } from '../Nav/NavHeader'
 
 export const Header: React.FC = () => {
   return (
-    <header className='w-full flex justify-between align-center py-2 px-4 shadow gap-2 bg-slate-50'>
+    <header className='w-full flex justify-between align-center py-2 px-4 shadow gap-6 bg-slate-50'>
       <SearchBar />
       <NavHeader />
     </header>
--- src/components/Nav/NavHeader/index.tsx
+++ src/components/Nav/NavHeader/index.tsx
@@ -4,12 +4,12 @@ import { MenuDropdown } from '../../MenuDropdown'
 
 export const NavHeader: React.FC = () => {
   return (
-    <nav className='max-w-fit ' role='navigation' aria-controls='navigation'>
-      <ul role='menu' className='flex flex-row gap-4 items-center'>
-        <li className='text-2xl'>
+    <nav className='max-w-fit' role='navigation' aria-controls='navigation'>
+      <ul role='menu' className='flex flex-row gap-6 items-center'>
+        <li className='text-2xl px-2'>
           <NotificationIcon />
         </li>
-        <li>
+        <li className=' px-2'>
           <Avatar />
         </li>
         <li>
