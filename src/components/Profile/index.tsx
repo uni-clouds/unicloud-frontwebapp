ad0c7705ddc22dae4d66d4313ef9b589b0a3ae81
joaolavelino <joaolavelino@gmail.com>
2022-07-07 16:09:57 -0300

    conflict fixes - 07-07

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,37 +1,35 @@
 import { Avatar } from '../Avatar'
 import { MdMoreVert } from 'react-icons/md'
 import { NavigationItem } from './NavigationItem'
-import { useEffect, useMemo, useState } from 'react'
-import PersonalInformation from './PersonalInformation'
+import { useState } from 'react'
 import { useUsersList } from '../../hooks/useUsersList'
 import { useDecode } from '../../hooks/useDecode'
 import { useUserData } from '../../hooks/useUserData'
+import { useNavigate } from 'react-router-dom'
 import { renderData } from './util'
-import { UserDataType } from '../Tables/Users/types'
+import { Link } from 'react-router-dom'
+import { useTranslation } from 'react-i18next'
+import PersonalInformation from './PersonalInformation'
 
 export const Profile: React.FC = () => {
   const [mode, setMode] = useState<'Informações Pessoais'>(
     'Informações Pessoais'
   )
-  const { data: customerData } = useUserData()
+  const { t } = useTranslation()
+
+  const { customerData } = useUserData()
   const { data } = useUsersList()
   const { user_id } = useDecode()
-  const [currentUser, setCurrentUser] = useState<UserDataType>()
-  const [isHugeName, setIsHugeName] = useState<'text-xl' | 'text-2xl'>(
-    'text-xl'
-  )
-  useEffect(() => {
-    const user = data?.filter((el) => el.id === user_id)[0]
-    setCurrentUser(user)
-    setIsHugeName(
-      customerData?.razao_social.length > 30 ? 'text-xl' : 'text-2xl'
-    )
-  }, [data, user_id, customerData])
+  const user = data?.filter((el) => el.id === user_id)[0]
+
+  const companyNameSize =
+    customerData?.razao_social.length > 30 ? 'text-xl' : 'text-2xl'
 
   function renderSection() {
     if (mode === 'Informações Pessoais')
-      return <PersonalInformation user={currentUser} />
+      return <PersonalInformation user={user} />
   }
+
   return (
     <section
       className='flex flex-1 border dark:border-none shadow bg-white custom-dark rounded-md'
@@ -41,16 +39,16 @@ export const Profile: React.FC = () => {
       <div className='flex flex-col max-w-64'>
         <div className='flex items-center justify-between w-full p-8 gap-4  '>
           <Avatar
-            firstName={currentUser ? renderData(currentUser.first_name) : ''}
-            lastName={currentUser ? renderData(currentUser.last_name) : ''}
+            firstName={user ? renderData(user.first_name) : ''}
+            lastName={user ? renderData(user.last_name) : ''}
           />
           <div className='flex-1'>
             <h3 className='font-black text-xl '>
-              {currentUser ? renderData(currentUser.first_name) : ''}{' '}
-              {currentUser ? renderData(currentUser.last_name) : ''}
+              {user ? renderData(user.first_name) : ''}{' '}
+              {user ? renderData(user.last_name) : ''}
             </h3>
             <h4 className='text-base-500'>
-              {currentUser ? renderData(currentUser.email) : ''}
+              {user ? renderData(user.email) : ''}
             </h4>
           </div>
           <button>
@@ -58,8 +56,8 @@ export const Profile: React.FC = () => {
           </button>
         </div>
         <div className='flex flex-col w-full p-8 border-t border-b dark:border-zinc-700'>
-          <h4>Empresa</h4>
-          <h2 className={`${isHugeName} font-bold`}>
+          <h4>{t('profile:company')}</h4>
+          <h2 className={`${companyNameSize} font-bold`}>
             {customerData ? renderData(customerData.razao_social) : ''}
           </h2>
         </div>
@@ -69,17 +67,25 @@ export const Profile: React.FC = () => {
             role='navigation'
             aria-label='User profile section'
           >
-            <ul className='flex flex-col w-full gap-8' role='menu'>
+            <ul className='flex flex-col w-full gap-8'>
               <NavigationItem
-                text='Informações Pessoais'
+                text={t('profile:personalInformation')}
                 href='#'
                 active={mode === 'Informações Pessoais'}
               />
 
-              <NavigationItem text='Notificações' href='#' active={false} />
-              <NavigationItem text='Atividade' href='#' active={false} />
               <NavigationItem
-                text='Configurações de Segurança'
+                text={t('profile:notifications')}
+                href='#'
+                active={false}
+              />
+              <NavigationItem
+                text={t('profile:activity')}
+                href='#'
+                active={false}
+              />
+              <NavigationItem
+                text={t('profile:safetyAdjusts')}
                 href='#'
                 active={false}
               />
@@ -88,7 +94,7 @@ export const Profile: React.FC = () => {
         </div>
       </div>
       <div className='flex flex-col flex-1 p-8 border-l dark:border-zinc-700'>
-        {currentUser ? renderSection() : null}
+        {user ? renderSection() : null}
       </div>
     </section>
   )
