240f57681379102a0f51997159b4f9e805455639
joaolavelino <joaolavelino@gmail.com>
2022-07-07 16:17:31 -0300

    conflict fixes - 07-07

--- src/pages/Pods/index.tsx
+++ src/pages/Pods/index.tsx
@@ -1,23 +1,12 @@
-import { Navigate } from 'react-router-dom'
 import { Layout } from '../../components/Layout'
-import { useCanAccess } from '../../hooks/useCanAccess'
 import { ZadaraPods } from '../../Templates/ZadaraPods'
 
 const Pods: React.FC = () => {
-  document.title = 'Uni.cloud | Pods'
-  const accessLevelZero = useCanAccess({
-    role: ['root']
-  })
-
-  if (accessLevelZero) {
-    return (
-      <Layout>
-        <ZadaraPods />
-      </Layout>
-    )
-  }
-
-  return <Navigate to='/not-found' replace />
+  return (
+    <Layout>
+      <ZadaraPods />
+    </Layout>
+  )
 }
 
 export default Pods
