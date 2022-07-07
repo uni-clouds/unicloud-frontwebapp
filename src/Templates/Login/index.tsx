fe8a1129365dc0b4d5777a4ddb4764b2571b34e4
joaolavelino <joaolavelino@gmail.com>
2022-07-07 16:13:41 -0300

    conflict fixes - 07-07

--- src/Templates/Login/index.tsx
+++ src/Templates/Login/index.tsx
@@ -13,11 +13,11 @@ export const Login: React.FC = () => {
   const navItems = [
     {
       name: `${translate('login:commercial')}`,
-      path: '/commercial'
+      path: 'https://uni.cloud/pt-br/fale-com-especialista/'
     },
     {
       name: `${translate('login:help-desk')}`,
-      path: '/help-desk'
+      path: 'https://uni.cloud/pt-br/fale-com-especialista/'
     }
   ]
   return (
@@ -25,7 +25,7 @@ export const Login: React.FC = () => {
       <main className='h-screen flex flex-col gap-6 items-center justify-center'>
         <section className='flex-col justify-center align-center'>
           <Logo />
-          <div className='mt-6 flex flex-col align-center w-[25rem] lg:w-[30rem] rounded-sm bg-neutral-50 custom-dark p-6 lg:p-10 justify-center border border-light-200 dark:border-neutral-700 shadow-sm'>
+          <div className='mt-6 flex gap-4 flex-col align-center w-[30rem] rounded-sm bg-neutral-50 custom-dark p-10 justify-center border border-light-200 dark:border-neutral-700 shadow-sm'>
             <div>
               <h4 className='font-bold text-2xl text-base-600 dark:text-base-200 mb-2'>
                 {translate('login:login')}
@@ -33,14 +33,10 @@ export const Login: React.FC = () => {
               <p className='text-sm'>{translate('login:subtitle')}</p>
             </div>
             <LoginForm />
-            <p className='my-5'>
-              {translate('login:noAccess')}
-              <span className='ml-2'>
-                <LinkInternal name={translate('login:noAccessLink')} href='#' />
-              </span>
-            </p>
-            <Divider>{translate('login:or')}</Divider>
-            <NavLinksHelpers items={navItems} />
+            <div className='flex flex-row items-center gap-2'>
+              <p className='text-sm'>{translate('login:noAccessLink')}</p>
+              <NavLinkLogin items={navItems} />
+            </div>
           </div>
         </section>
         <Footer local='auth' />
