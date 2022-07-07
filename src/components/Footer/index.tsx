7c1a86b7842ca05eb8e5a36322efa51abc404784
joaolavelino <joaolavelino@gmail.com>
2022-07-07 15:28:57 -0300

    conflict fixes - 07-07

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -26,8 +26,8 @@ export const Footer = ({ local }: { local: string }) => {
       className={`flex items-center flex-wrap p-4 justify-around border-t border-light-200 bg-slate-50 dark:bg-zinc-800 dark:border-neutral-700
       ${local === 'auth' ? 'fixed inset-x-0 bottom-0 w-full ' : 'relative'}`}
     >
-      <div className='text-base-400 dark:text-base-100 text-sm mx-auto'>
-        &copy; {t('copyright')}.
+      <div className='text-base-400 dark:text-base-100 text-sm'>
+        &copy; 2022 Uni.Cloud. Todos os direitos reservados.
       </div>
       <nav>
         <NavLinksHelpers items={navItems} />
