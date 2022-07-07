17282c93e800815451817877b679aea823b55f27
joaolavelino <joaolavelino@gmail.com>
2022-07-07 15:31:36 -0300

    conflict fixes - 07-07

--- src/Templates/Login/index.tsx
+++ src/Templates/Login/index.tsx
@@ -21,18 +21,22 @@ export const Login: React.FC = () => {
       <main className='h-screen flex flex-col gap-6 items-center justify-center'>
         <section className='flex-col justify-center align-center'>
           <Logo />
-          <div className='mt-6 flex gap-4 flex-col align-center w-[30rem] rounded-sm bg-neutral-50 custom-dark p-10 justify-center border border-light-200 dark:border-neutral-700 shadow-sm'>
+          <div className='mt-6 flex flex-col align-center w-[25rem] lg:w-[30rem] rounded-sm bg-neutral-50 custom-dark p-6 lg:p-10 justify-center border border-light-200 dark:border-neutral-700 shadow-sm'>
             <div>
-              <h4 className='font-bold text-2xl text-base-600 dark:text-base-200 mb-2'>
-                {translate('login:login')}
+              <h4 className='font-bold text-xl lg:text-2xl text-base-600 dark:text-base-200 mb-2'>
+                Entrar
               </h4>
               <p className='text-sm'>{translate('login:subtitle')}</p>
             </div>
             <LoginForm />
-            <div className='flex flex-row items-center gap-2'>
-              <p className='text-sm'>Ainda não possui acesso?</p>
-              <NavLinkLogin items={navItems} />
-            </div>
+            <p className='my-4 text-sm lg:text-md text-center'>
+              Ainda não possui acesso?
+              <span className='ml-2'>
+                <LinkInternal name='Contate nossa equipe' href='#' />
+              </span>
+            </p>
+            <Divider>OU</Divider>
+            <NavLinksHelpers items={navItems} />
           </div>
         </section>
         <Footer local='auth' />
