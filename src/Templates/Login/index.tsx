a8c436a0cb89258f56d0a594b42dba3689d29baa
joaolavelino <joaolavelino@gmail.com>
2022-07-08 11:48:33 -0300

    git pull 8/7

--- src/Templates/Login/index.tsx
+++ src/Templates/Login/index.tsx
@@ -3,6 +3,9 @@ import { Login as LoginForm } from '../../components/Forms/Login'
 import { Logo } from '../../components/Logo'
 import { NavLinkLogin } from '../../components/Elements/Navigation/NavLinkLogin'
 import { useTranslation } from 'react-i18next'
+import { LinkInternal } from '../../components/Elements/Navigation/LinkInternal'
+import { Divider } from '@mui/material'
+import { NavLinksHelpers } from '../../components/Elements/Navigation/NavLinksHelpers'
 
 export const Login: React.FC = () => {
   const { t: translate } = useTranslation()
@@ -22,18 +25,22 @@ export const Login: React.FC = () => {
       <main className='h-screen flex flex-col gap-6 items-center justify-center'>
         <section className='flex-col justify-center align-center'>
           <Logo />
-          <div className='mt-6 flex gap-4 flex-col align-center w-[30rem] rounded-sm bg-neutral-50 custom-dark p-10 justify-center border border-light-200 dark:border-neutral-700 shadow-sm'>
+          <div className='mt-6 flex flex-col align-center w-[25rem] lg:w-[30rem] rounded-sm bg-neutral-50 custom-dark p-6 lg:p-10 justify-center border border-light-200 dark:border-neutral-700 shadow-sm'>
             <div>
-              <h4 className='font-bold text-2xl text-base-600 dark:text-base-200 mb-2'>
+              <h4 className='font-bold text-xl lg:text-2xl text-base-600 dark:text-base-200 mb-2'>
                 {translate('login:login')}
               </h4>
               <p className='text-sm'>{translate('login:subtitle')}</p>
             </div>
             <LoginForm />
-            <div className='flex flex-row items-center gap-2'>
-              <p className='text-sm'>{translate('login:noAccessLink')}</p>
-              <NavLinkLogin items={navItems} />
-            </div>
+            <p className='my-4 text-sm lg:text-md text-center'>
+              {translate('login:noAccess')}
+              <span className='ml-2'>
+                <LinkInternal name={translate('login:noAccessLink')} href='#' />
+              </span>
+            </p>
+            <Divider>{translate('login:or')}</Divider>
+            <NavLinksHelpers items={navItems} />
           </div>
         </section>
         <Footer local='auth' />
