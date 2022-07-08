f011eedb6e0145dae4d78fe77e7aa63088c9501d
joaolavelino <joaolavelino@gmail.com>
2022-07-08 10:06:36 -0300

    git pull 8/7

--- src/Templates/ZadaraPods/index.tsx
+++ src/Templates/ZadaraPods/index.tsx
@@ -4,19 +4,5 @@ import { usePodsData } from '../../hooks/usePodsData'
 import { Header } from './Header'
 
 export const ZadaraPods: React.FC = () => {
-  const { data } = usePodsData()
-  const { breakpoints } = useTheme()
-  const mediaQuery = useMediaQuery(breakpoints.down('lg'))
-  const isFullScreen = Number(data?.length) > 6 && mediaQuery === true
-
-  return (
-    <section
-      className={`container mx-auto ${
-        isFullScreen ? ' h-full' : 'h-screen'
-      } px-12 flex flex-col gap-4`}
-    >
-      <Header pods={Number(data?.length)} />
-      <CardPod />
-    </section>
-  )
+  return <h1>Pods aqui!!!!</h1>
 }
