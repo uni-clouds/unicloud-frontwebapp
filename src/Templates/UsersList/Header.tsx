86b2805d7a4ea19d4079dddc007755522ddc14ce
joaolavelino <joaolavelino@gmail.com>
2022-07-08 11:52:39 -0300

    git pull 8/7

--- src/Templates/UsersList/Header.tsx
+++ src/Templates/UsersList/Header.tsx
@@ -48,8 +48,8 @@ export const Header: React.FC<HeaderProps> = ({ totalUsers, data }) => {
             name='adicionar usuários'
             onclick={() => setOpenModal(true)}
           >
-            {translate('customersUsers:addUser')}
             <HiPlus fontSize={20} />
+            {translate('user')}
           </PurpleButton>
         </Tooltip>
         <ModalInvitation
