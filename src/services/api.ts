af93fd375e8d990c16e84feaa03b9617190621e1
Emanuela Coutinho <gdn.sasha@gmail.com>
2022-07-08 13:00:39 -0300

    switch url_base

--- src/services/api.ts
+++ src/services/api.ts
@@ -8,9 +8,9 @@ let failedRequestQueue: any = []
 let URL_BACKEND = 'https://unicloudbr.azurewebsites.net'
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL
+  baseURL: URL_BACKEND
 })
-console.log('🎯', import.meta.env.VITE_BASE_URL)
+
 
 api.interceptors.request.use(
   (config: AxiosRequestConfig) => {
