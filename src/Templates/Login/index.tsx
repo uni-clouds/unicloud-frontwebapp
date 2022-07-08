4d72b4654a2d9a7f2cb02df998527e963317f60c
joaolavelino <joaolavelino@gmail.com>
2022-07-08 10:05:14 -0300

    git pull 8/7

--- src/Templates/Login/index.tsx
+++ src/Templates/Login/index.tsx
@@ -1,22 +1,19 @@
 import { Footer } from '../../components/Footer'
 import { Login as LoginForm } from '../../components/Forms/Login'
 import { Logo } from '../../components/Logo'
-import { LinkInternal } from '../../components/Elements/Navigation/LinkInternal'
-import { NavLinksHelpers } from '../../components/Elements/Navigation/NavLinksHelpers'
-import { Divider } from '@mui/material'
-import { useTranslation } from 'react-i18next'
+import { NavLinkLogin } from '../../components/Elements/Navigation/NavLinkLogin'
 
 export const Login: React.FC = () => {
   const { t: translate } = useTranslation()
 
   const navItems = [
     {
-      name: `${translate('login:commercial')}`,
-      path: '/commercial'
+      name: 'Comercial',
+      path: 'https://uni.cloud/pt-br/fale-com-especialista/'
     },
     {
-      name: `${translate('login:help-desk')}`,
-      path: '/help-desk'
+      name: 'Suporte Técnico',
+      path: 'https://uni.cloud/pt-br/fale-com-especialista/'
     }
   ]
   return (
@@ -24,7 +21,7 @@ export const Login: React.FC = () => {
       <main className='h-screen flex flex-col gap-6 items-center justify-center'>
         <section className='flex-col justify-center align-center'>
           <Logo />
-          <div className='mt-6 flex flex-col align-center w-[25rem] lg:w-[30rem] rounded-sm bg-neutral-50 custom-dark p-6 lg:p-10 justify-center border border-light-200 dark:border-neutral-700 shadow-sm'>
+          <div className='mt-6 flex gap-4 flex-col align-center w-[30rem] rounded-sm bg-neutral-50 custom-dark p-10 justify-center border border-light-200 dark:border-neutral-700 shadow-sm'>
             <div>
               <h4 className='font-bold text-2xl text-base-600 dark:text-base-200 mb-2'>
                 {translate('login:login')}
@@ -32,14 +29,10 @@ export const Login: React.FC = () => {
               <p className='text-sm'>{translate('login:subtitle')}</p>
             </div>
             <LoginForm />
-            <p className='my-4 text-sm lg:text-md text-center'>
-              {translate('login:noAccess')}
-              <span className='ml-2'>
-                <LinkInternal name={translate('login:noAccessLink')} href='#' />
-              </span>
-            </p>
-            <Divider>OU</Divider>
-            <NavLinksHelpers items={navItems} />
+            <div className='flex flex-row items-center gap-2'>
+              <p className='text-sm'>Ainda não possui acesso?</p>
+              <NavLinkLogin items={navItems} />
+            </div>
           </div>
         </section>
         <Footer local='auth' />
