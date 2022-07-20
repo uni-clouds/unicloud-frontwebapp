2f09e06e47f9eaf8a244b8082893a043b184105c
ManuCoutinho <gdn.sasha@gmail.com>
2022-07-20 08:29:57 -0300

    feat: :sparkles: created new hook access zero

--- src/pages/Contracts/index.tsx
+++ src/pages/Contracts/index.tsx
@@ -1,9 +1,17 @@
 import { FC } from 'react'
+import { Navigate } from 'react-router-dom'
+import { useAccessLevelZero } from '../../hooks/useAccessLevelZero'
 import { ContractsPage } from '../../templates/Contracts'
 
 const Contracts: FC = () => {
-  //level zero add
-  return <ContractsPage />
+  document.title = 'Uni.cloud | Recursos'
+  const accessLevelZero = useAccessLevelZero('root')
+
+  if (accessLevelZero) {
+    return <ContractsPage />
+  }
+
+  return <Navigate to='/not-found' replace />
 }
 
 export default Contracts
--- src/pages/Resources/index.tsx
+++ src/pages/Resources/index.tsx
@@ -1,9 +1,19 @@
 import { FC } from 'react'
+import { Navigate } from 'react-router-dom'
+import { useCanAccess } from '../../hooks/useCanAccess'
 import { ResourcesPage } from '../../templates/Resources'
 
 const Resources: FC = () => {
-  //level zero
-  return <ResourcesPage />
+  document.title = 'Uni.cloud | Recursos'
+  const accessLevelZero = useCanAccess({
+    role: ['root']
+  })
+
+  if (accessLevelZero) {
+    return <ResourcesPage />
+  }
+
+  return <Navigate to='/not-found' replace />
 }
 
 export default Resources
